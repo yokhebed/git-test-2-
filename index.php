edcbe9c9c0e181f5b658637623a9c4a53be12af9
Marchellino <marchellino940@gmail.com>
2026-02-08 16:34:51 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     </head>
     <body>
         <h3> Hi! </h3>
-        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui architecto aut doloribus minima asperiores voluptatem pariatur? Nisi libero ut aperiam soluta. Voluptatem, animi suscipit ab eos eveniet quod quibusdam reiciendis?</p>
+        <p> Lorem ipsum dolor
+        <p> wle </p>
     </body>
 </html>
