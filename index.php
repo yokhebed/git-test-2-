16f79646d41c300993d43c728ca8a7074ab1814e
Marchellino <marchellino940@gmail.com>
2026-02-08 16:33:40 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+!DOCTYPE <html>
+    html lang="en">
+    <head>
+        <meta charset="UTF=8">
+        <title> Coba Git Remote </title>
+    </head>
+    <body>
+        <h3> Hi! </h3>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui architecto aut doloribus minima asperiores voluptatem pariatur? Nisi libero ut aperiam soluta. Voluptatem, animi suscipit ab eos eveniet quod quibusdam reiciendis?</p>
+    </body>
+</html>
